43dd3cf1aceff5abcdd46f8d71e909f261d3f7c5
standchan <standchan@foxmail.com>
2024-06-12 08:20:58 +0800

    add some func

--- zlm_exporter.go
+++ zlm_exporter.go
@@ -1,12 +1,88 @@
 package main
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"io"
+	"net/http"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	namespace = "zlmediakit"
+)
+
+var (
+	serverLabelNames = []string{"backend", "server"}
+)
 
 type metricInfo struct {
 	Desc *prometheus.Desc
 	Type prometheus.ValueType
 }
 
+type metrics map[int]metricInfo
+
+func (m metrics) String() string {
+	keys := make([]int, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	s := make([]string, len(keys))
+	for i, k := range keys {
+		s[i] = strconv.Itoa(k)
+	}
+	return strings.Join(s, ",")
+}
+
+var (
+	serverMetrics = metrics{
+		1: newServerMetric("threads_load"),
+	}
+)
+
+func newServerMetric(metricName string, docString string, t prometheus.ValueType, constLabels prometheus.Labels) metricInfo {
+	return metricInfo{
+		Desc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "server", metricName),
+			docString,
+			serverLabelNames,
+			constLabels,
+		),
+		Type: t,
+	}
+}
+
+func fetchHTTP(uri string, sslVerify, proxyFromEnv bool, timeout time.Duration) func() (io.ReadCloser, error) {
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: !sslVerify}}
+	if proxyFromEnv {
+		tr.Proxy = http.ProxyFromEnvironment
+	}
+	client := http.Client{
+		Timeout:   timeout,
+		Transport: tr,
+	}
+
+	return func() (io.ReadCloser, error) {
+		resp, err := client.Get(uri)
+		if err != nil {
+			return nil, err
+		}
+		if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
+			resp.Body.Close()
+			return nil, fmt.Errorf("HTTP status %d", resp.StatusCode)
+		}
+		return resp.Body, nil
+	}
+}
+
+// doc: https://prometheus.io/docs/instrumenting/writing_exporters/
+// 1.metric must use base units
 func main() {
 
 }
